de93811d14cee653beb5a32b26e5477c1afc159c
shawal106khan <shawal106khan@example.com>
2026-01-03 06:44:17 +0500

    Setup React Vite project with Tailwind CSS v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 function App() {
-  return <h1>DOCTOR RECOMMENDATION APP</h1>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Doctor Recommendation System
+      </h1>
+    </div>
+  );
 }
+
 export default App;
